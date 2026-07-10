c1aafef0cf14f4be5f9d18f5da80e48b33765435
Nihar Turlapati <nturlapa@purdue.edu>
2026-07-10 15:07:41 -0400

    Add: binary-search - submission-3

---
+++ Data Structures & Algorithms/binary-search/submission-3.py
@@ -0,0 +1,19 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        # sorted in ascending order
+        left = 0
+        right = len(nums)-1
+        
+        while left <= right:
+
+            mid = left + (right - left) // 2
+            print(mid)
+            if nums[mid] < target:
+                left = mid+1
+            
+            elif nums[mid] > target:
+                right = mid-1
+            
+            else:
+                return mid
+        return -1
