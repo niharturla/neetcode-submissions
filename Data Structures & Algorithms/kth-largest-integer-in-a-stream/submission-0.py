e58b4b1a29217fdf4e3ed71944c16f5d41a4c299
Nihar Turlapati <nturlapa@purdue.edu>
2026-09-13 20:09:38 -0400

    Add: kth-largest-integer-in-a-stream - submission-0

---
+++ Data Structures & Algorithms/kth-largest-integer-in-a-stream/submission-0.py
@@ -0,0 +1,15 @@
+import heapq
+
+class KthLargest:
+    
+    def __init__(self, k: int, nums: List[int]):
+        self.nums = sorted(nums)
+        self.k = k
+
+    def add(self, val: int) -> int:
+        self.nums.append(val)
+        self.nums.sort()
+        return self.nums[-self.k]
+       
+
+        
